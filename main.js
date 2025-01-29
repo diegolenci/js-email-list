2110906dc714dd4164e16a2a88ae02c43beef7ea
diegolenci <lencidiego0@gmail.com>
2025-01-29 09:43:23 +0100

    correzione: lista email stampata in pagina in una lista

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+const listaEmail = document.getElementById('email')
 
 for(let i = 1; i < 11; i++){
-    
+
     fetch("https://flynn.boolean.careers/exercises/api/random/mail")
 .then(response => response.json())
 .then(data => {
     console.log(data.response)
+    listaEmail.innerHTML += `<li>${data.response}</li>`
 })
 .catch(Error => {
 console.error(Error);
 })
 }
+
+
